dea14007b6b2b63d4ef4091656c3234a7c751b6d
MartinMSalas <ingmartinmsalas@gmail.com>
2025-05-20 09:17:42 -0300

    feat<BeerData>:add Beer model. Closes #3

---
+++ src/main/java/com/mmstechnology/mssc_beer_service/web/model/BeerRecord.java
@@ -0,0 +1,16 @@
+package com.mmstechnology.mssc_beer_service.web.model;
+
+
+import lombok.Builder;
+
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+
+@Builder
+public record BeerRecord(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate,
+                         String beerName, BeerStyleEnum beerStyle, Long upc, BigDecimal price, Integer quantityOnHand){
+
+}
